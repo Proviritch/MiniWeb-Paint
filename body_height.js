487514bd2e0d81fa5dba9ec5c50ee96dd716d91e
Adir <adirelpro@gmail.com>
2022-09-30 17:41:08 -0500

    modificado el tamaño de drawToolSection y el tamaño de main además de overflow hidden para el body

--- body_height.js
+++ body_height.js
@@ -1,10 +1,12 @@
 let height;
 let body = document.querySelector('body');
+let main = document.querySelector('main')
 
 const callback = (entries) => {
     console.log(entries);
     height = entries[0].rootBounds.height;
     body.style.height = height + 'px';
+    main.style.height = height + 'px';
 }
 
 const options = {
